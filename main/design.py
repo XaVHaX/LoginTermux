b9db3bb285c84e688e36f3315216b0133eee1cc4
xavhax <62423022+XaVHaX@users.noreply.github.com>
2022-04-13 17:17:30 +0700

    Add files via upload

---
+++ main/design.py
@@ -0,0 +1,6 @@
+from main.color import *
+
+ask = R + '[' + Y + '?' + R + ']' + WM
+check = R + "[" + G + "√" + R + "]" + WM
+warn = G + "[" + R + "!" + G + "]" + WM
+info = G + "[" + W + "*" + G + "]" + WM
